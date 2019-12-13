918c0bbdadd98df92f77f4c6acd4eb7b5b148a65
Chelina Ortiz Montanez <37750291+omchel@users.noreply.github.com>
2019-12-12 19:29:45 -0500

    Update utilities.js

--- src/main/utilities.js
+++ src/main/utilities.js
@@ -7,6 +7,15 @@ const utilities = {
       items.splice(randomIndex, 1);
     }
     return randomItems;
+  },
+  randomSelect: (items, numItems) => {
+    var randomItems = [];
+    while (randomItems.length !== numItems) {
+      randomIndex = Math.floor(Math.random() * items.length);
+      randomItems.push(items[randomIndex]);
+      items.splice(randomIndex, 1);
+    }
+    return randomItems;
   }
 };
 
